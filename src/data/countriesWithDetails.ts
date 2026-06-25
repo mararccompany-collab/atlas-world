a6ef62874ccec6d45ace055aa9a78213fce13de2
mararccompany-collab <mararc.company@gmail.com>
2026-06-25 16:35:25 +0300

    Add files via upload

--- src/data/countriesWithDetails.ts
+++ src/data/countriesWithDetails.ts
@@ -2,7 +2,7 @@ import { countries, Country } from './countries';
 import { enrichmentData } from './countries-enriched';
 import { extraDetailsData } from './countries-extra-details';
 import { statisticsData } from './countries-statistics';
-import { anthemUrls } from './anthemUrls';
+import { getAnthemUrl } from './anthemUrls';
 import { leaderImages } from './leaderImages';
 import { arabExtraInfo } from './arabExtraInfo';
 
@@ -11,7 +11,7 @@ function enrichCountries(): Country[] {
     const extra = enrichmentData[c.id];
     const extra2 = extraDetailsData[c.id];
     const extra3 = statisticsData[c.id];
-    const anthemUrl = anthemUrls[c.id];
+    const anthemUrl = getAnthemUrl(c.id);
     const leaderImg = leaderImages[c.id];
     const extraInfo = arabExtraInfo[c.id];
     let result = { ...c };
